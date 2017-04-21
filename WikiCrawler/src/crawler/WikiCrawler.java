758c72b855b751410d70b4214c2316d782d2a12d
Mrluigifan102 <Mrluigifan102@gmail.com>
2017-04-21 12:26:52 +0200

    Structure Overhaul
    Makes UrlPriority Runnable and moves readPage logic to UrlPriority's
    run() method.

--- WikiCrawler/src/crawler/WikiCrawler.java
+++ WikiCrawler/src/crawler/WikiCrawler.java
@@ -6,8 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.PriorityQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -16,11 +15,15 @@ import java.util.concurrent.TimeUnit;
  */
 public class WikiCrawler {
     private static final Object key = new Object();
-    private PriorityQueue<UrlPriority> urls = new PriorityQueue<>((o1, o2) -> o2.priority - o1.priority);
-    private ThreadPoolExecutor tPool = new ThreadPoolExecutor(5, 10, 50000L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+    private ThreadPoolExecutor tPool = new ThreadPoolExecutor(5, 10,
+            50000L, TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>(11,
+            (o1, o2) -> {
+                if (!(o1 instanceof UrlPriority) || !(o2 instanceof UrlPriority)) return 0;
+                return ((UrlPriority) o1).compareTo(((UrlPriority) o2));
+            }));
     private byte steps = 3;
 
-    private void readPage(String url, byte step) {
+    /*private void readPage(String url, byte step) {
         //Get document from url. Make thread sleep so only 1 page can be received per second, as demanded by Wikipedia.
         Document doc;
         synchronized (key) {
@@ -45,9 +48,9 @@ public class WikiCrawler {
             if (href.equals("") || !href.startsWith("/wiki/")) continue; //Go to next link if url doesn't exist or couldn't be found or leaves Wikipedia.
             if (step < steps) urls.add(new UrlPriority("https://en.wikipedia.org" + href, (byte)(step+1))); //Add page to priority queue if next step is within upper bound.
         }
-    }
+    }*/
 
-    private class UrlPriority implements Comparable<UrlPriority> {
+    private class UrlPriority implements Comparable<UrlPriority>, Runnable {
         private String url;
         private int priority;
         private byte step;
@@ -66,6 +69,34 @@ public class WikiCrawler {
         @Override
         public int compareTo(UrlPriority o) {
             return o.priority - this.priority;
+        }
+
+        @Override
+        public void run() {
+            //Get document from url. Make thread sleep so only 1 page can be received per second, as demanded by Wikipedia.
+            Document doc;
+            synchronized (key) {
+                try {
+                    doc = Jsoup.connect(url).get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Element main = doc.body().select("div#bodyContent").first(); //Get all div tags that match id "bodyContent". Only one could match, so first is chosen.
+            if (main == null) return; //Return if no text exists or text couldn't be found.
+            Elements links = main.select("a"); //Get all a tags in the text.
+            for (Element link : links) {
+                String href = link.attr("href"); //Get url from link.
+                if (href.equals("") || !href.startsWith("/wiki/")) continue; //Go to next link if url doesn't exist or couldn't be found or leaves Wikipedia.
+                if (step < steps) tPool.submit(new UrlPriority("https://en.wikipedia.org" + href, (byte)(step+1))); //Add page to threadpool if next step is within upper bound.
+            }
         }
     }
 }
