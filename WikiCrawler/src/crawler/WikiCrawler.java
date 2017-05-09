8c16ca32a60ddf3a2270b16d205ebebd4f133bc3
Mrluigifan102 <Mrluigifan102@gmail.com>
2017-05-09 22:40:08 +0200

    Rewrite
    Rewrite of the crawler to be more in line with new plans.

--- WikiCrawler/src/crawler/WikiCrawler.java
+++ WikiCrawler/src/crawler/WikiCrawler.java
@@ -8,11 +8,8 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -21,45 +18,41 @@ import java.util.concurrent.TimeUnit;
  */
 public class WikiCrawler {
     private static final Object key = new Object();
-    private ThreadPoolExecutor tPool = new ThreadPoolExecutor(5, 10, 50000L, TimeUnit.MILLISECONDS,
-            new PriorityBlockingQueue<>(11, (o1, o2) -> {
-                if (!(o1 instanceof WikiPage) || !(o2 instanceof WikiPage))
-                    return 0;
-                return ((WikiPage) o1).compareTo((WikiPage) o2);
-            }));
+    private ThreadPoolExecutor tPool = new ThreadPoolExecutor(5, 10, 50000L,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
     private final byte steps;
-    private String end;
-    private List<String> sequence = null;
-    private Set<String> visited = new HashSet<>();
+    private Set<String> visitedOut = new HashSet<>();
+    private Set<String> visitedIn = new HashSet<>();
+    private Map<String, List<String>> allAdj = new HashMap<>();
 
     public WikiCrawler() {
         this((byte) 3);
     }
 
     public WikiCrawler(byte steps) {
-        this("https://en.wikipedia.org/wiki/thing", steps, "https://en.wikipedia.org/wiki/New_Haven,_Connecticut");
+        this("https://en.wikipedia.org/wiki/thing", steps);
     }
 
-    public WikiCrawler(String start, byte steps, String end) {
+    public WikiCrawler(String start, byte steps) {
         this.steps = (byte) Math.min(steps, 6);
-        synchronized (key) {
+        tPool.submit(() -> {
             try {
-                tPool.submit(new WikiPage(start, Jsoup.connect(start).get(), (byte) 0, new LinkedList<>()));
-            } catch (IOException e) {
+                readPageOut(URLDecoder.decode(start, "UTF-8"), (byte) 0);
+            } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
+        });
+        tPool.submit(() -> {
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                readPageIn(URLDecoder.decode(start, "UTF-8"), (byte) 0);
+            } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-        }
-        try {
-            this.end = URLDecoder.decode(end, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            this.end = end;
-        }
+        });
+    }
+
+    public Map<String, List<String>> getAllAdj() {
+        return new LinkedHashMap<>(allAdj);
     }
 
     private static long time;
@@ -67,100 +60,127 @@ public class WikiCrawler {
 
     public static void main(String[] args) {
         time = System.currentTimeMillis();
-        WikiCrawler c = new WikiCrawler("https://en.wikipedia.org/wiki/Special:Random", (byte) 1,
-                "https://en.wikipedia.org/wiki/Lee_Chiao-ju");
+        WikiCrawler c = new WikiCrawler("https://en.wikipedia.org/wiki/Special:Random", (byte) 0);
     }
 
     private void stop() {
-        System.out.println("Time: " + (System.currentTimeMillis() - time) + " Pages: " + counter + " Finished: " + (sequence != null));
+        System.out.println("Time: " + (System.currentTimeMillis() - time) + " Pages: " + counter);
     }
 
-    private class WikiPage implements Comparable<WikiPage>, Runnable {
-        private String url;
-        private Document doc;
-        private int priority;
-        private byte step;
-        private List<String> previous;
+    private void readPageOut(String url, byte step) {
+        counter++;
+        System.out.println(url.substring(30));
 
-        private WikiPage(String url, Document doc, byte step, List<String> previous) {
-            this.url = url;
-            this.doc = doc;
-            this.priority = calcPriority();
-            this.step = step;
-            this.previous = previous;
-        }
-
-        private int calcPriority() {
-            if (step >= steps) return Integer.MIN_VALUE;
-            Element mainText = doc.select("div#bodyContent").first();
-            if (mainText == null)
-                return Integer.MIN_VALUE;
-            Elements links = mainText.select("a");
-            return links.size();
-        }
-
-        @Override
-        public int compareTo(WikiPage o) {
-            return o.priority - this.priority;
-        }
-
-        @Override
-        public void run() {
-            counter++;
-
-            previous.add(doc.title()); //Add current url to the sequence.
-
-            //Check if the current page is the one being searched for.
-            if (url.equalsIgnoreCase(end)) {
-                if (sequence == null || sequence.size() > previous.size()) sequence = previous;
+        Document page;
+        synchronized (key) {
+            try {
+                page = Jsoup.connect(url).get();
+            } catch (IOException e) {
+                e.printStackTrace();
                 return;
             }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-            Element main = doc.body().select("div#bodyContent").first(); //Get all div tags that match id "bodyContent". Only one could match, so first is chosen.
-            if (main == null) return; //Return if no text exists or text couldn't be found.
-            Elements links = main.select("a"); //Get all a tags in the text.
-            for (Element link : links) {
-                if (step >= steps) break;
-                String href = link.attr("href"); //Get url from link.
+        List<String> adjacent = new LinkedList<>(); //List to hold adjacent pages.
 
-                //Go to next link if url doesn't exist, couldn't be found, leaves Wikipedia, or goes to a page type we don't want.
-                if (href.equals("") || !href.startsWith("/wiki/") || href.startsWith("/wiki/File:")
-                        || href.startsWith("/wiki/Portal:") || href.startsWith("/wiki/Help:")
-                        || href.startsWith("/wiki/Template:") || href.startsWith("/wiki/Category:")
-                        || href.startsWith("/wiki/Template_talk:")) continue;
-
-                try {
-                    href = URLDecoder.decode(href, doc.charset().displayName()); //Translate unicode to actual characters using charset of page.
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
-                if (visited.contains(href)) continue;
-                visited.add(href);
-
-                Document linkPage;
-                synchronized (key) {
-                    try {
-                        linkPage = Jsoup.connect("https://en.wikipedia.org" + href).get();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        continue;
-                    }
-                    System.out.println(href + " " + step);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                tPool.submit(new WikiPage("https://en.wikipedia.org" + href, linkPage, (byte)(step+1),
-                        new LinkedList<>(previous))); //Add page to threadpool if next step is within upper bound.
+        Element main = page.body().select("div#bodyContent").first(); //Get all div tags that match id "bodyContent". Only one could match, so first is chosen.
+        if (main == null) return; //Return if no text exists or text couldn't be found.
+        Elements links = main.select("a"); //Get all a tags in the text.
+        for (Element link : links) {
+            String href; //Get url from link.
+            try {
+                href = URLDecoder.decode(link.attr("href"), page.charset().displayName());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                continue;
             }
 
-            if (step == steps && tPool.getQueue().isEmpty()) {
-                stop();
-                tPool.shutdown();
+            //Go to next link if url doesn't exist, couldn't be found, leaves Wikipedia, or goes to a page type we don't want.
+            if (href.equals("") || !href.startsWith("/wiki/") || href.startsWith("/wiki/File:")
+                    || href.startsWith("/wiki/Portal:") || href.startsWith("/wiki/Help:")
+                    || href.startsWith("/wiki/Template:") || href.startsWith("/wiki/Category:")
+                    || href.startsWith("/wiki/Template_talk:") || href.startsWith("/w/")) continue;
+
+            adjacent.add("https://en.wikipedia.org" + href);
+
+            if (visitedOut.contains(href)) continue;
+
+            if (step < steps) {
+                tPool.submit(() -> readPageOut("https://en.wikipedia.org" + href, (byte)(step+1))); //Add page to threadpool if next step is within upper bound.
+                visitedOut.add(href);
             }
+        }
+
+        if (step == steps && tPool.getQueue().isEmpty()) {
+            stop();
+            if (!tPool.isShutdown()) tPool.shutdown();
+        }
+
+        if (allAdj.containsKey(url)) allAdj.get(url).addAll(adjacent);
+        else allAdj.put(url, adjacent);
+    }
+
+    private void readPageIn(String url, byte step) {
+        counter++;
+
+        Document page;
+        synchronized (key) {
+            try {
+                page = Jsoup.connect(
+                        "https://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/"
+                                + url.substring(30)
+                                + "&limit=5000"
+                ).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Element main = page.body().select("ul#mw-whatlinkshere-list").first(); //Get all ul tags that match id "mw-whatlinkshere-list". Only one could match, so first is chosen.
+        if (main == null) return; //Return if no list exists or list couldn't be found.
+        Elements links = main.select("a"); //Get all a tags in the text.
+        for (Element link : links) {
+            String href; //Get url from link.
+            try {
+                href = URLDecoder.decode(link.attr("href"), page.charset().displayName());
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            //Go to next link if url doesn't exist, couldn't be found, leaves Wikipedia, or goes to a page type we don't want.
+            if (href.equals("") || !href.startsWith("/wiki/") || href.startsWith("/wiki/File:")
+                    || href.startsWith("/wiki/Portal:") || href.startsWith("/wiki/Help:")
+                    || href.startsWith("/wiki/Template:") || href.startsWith("/wiki/Category:")
+                    || href.startsWith("/wiki/Template_talk:") || href.startsWith("/w/")) continue;
+
+            if (visitedIn.contains(href)) continue;
+
+            if (step < steps) {
+                tPool.submit(() -> readPageIn("https://en.wikipedia.org" + href, (byte)(step+1))); //Add page to threadpool if next step is within upper bound.
+                visitedIn.add(href);
+            }
+
+            List<String> adj = new LinkedList<>();
+            adj.add(url);
+            if (allAdj.containsKey("https://en.wikipedia.org" + href)) allAdj.get("https://en.wikipedia.org" + href).addAll(adj);
+            else allAdj.put("https://en.wikipedia.org" + href, adj);
+        }
+
+        if (step == steps && tPool.getQueue().isEmpty()) {
+            stop();
+            if (!tPool.isShutdown()) tPool.shutdown();
         }
     }
 }
